cfb43c4ab3531353a09d9d1a7ed5277533b011bc
Szu-Xiang <54528055+Szu-Xiang@users.noreply.github.com>
2022-04-08 19:13:53 +0800

    Add files via upload

---
+++ Day1/Test.java
@@ -0,0 +1,10 @@
+package ConstentDemo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("被称为一次编写，处处运行");
+        System.out.println(123);
+        System.out.println('a');
+    }
+}
